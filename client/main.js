685a9c0d863056a0e6205a9525663d15e94a4613
CathalRyan96 <cathalryan48@gmail.com>
2018-11-14 11:54:06 +0000

    basic react component now working

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Meteor } from "meteor/meteor";
+import "./main.html";
 
 Meteor.startup(function _startup() {
   let title = "Test App!";
@@ -10,6 +11,7 @@ Meteor.startup(function _startup() {
     <div>
       <h2>{title}</h2>
       <p>My name is {name}</p>
+      <p>test</p>
     </div>
   );
 
