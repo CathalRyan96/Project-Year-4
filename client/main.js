d07f5922a77a71bc9fb31abe2c781e75da0a4c80
CathalRyan96 <cathalryan48@gmail.com>
2018-11-14 11:39:49 +0000

    trying to add react component

--- client/main.js
+++ client/main.js
@@ -1,1 +1,17 @@
-console.log("server.js");
+import React from "react";
+import ReactDOM from "react-dom";
+import { Meteor } from "meteor/meteor";
+
+Meteor.startup(function _startup() {
+  let title = "Test App!";
+  let name = "Cathal";
+
+  let html = (
+    <div>
+      <h2>{title}</h2>
+      <p>My name is {name}</p>
+    </div>
+  );
+
+  ReactDOM.render(html, document.getElementById("app"));
+});
