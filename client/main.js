c9a619eee536a8f93781da3e3a5e4b5b632f1e18
CathalRyan96 <cathalryan48@gmail.com>
2018-11-14 12:23:47 +0000

    outputting hardcoded list to server

--- client/main.js
+++ client/main.js
@@ -3,15 +3,38 @@ import ReactDOM from "react-dom";
 import { Meteor } from "meteor/meteor";
 import "./main.html";
 
+const LIST = [
+  { id: 1, name: "Tom", score: 10 }, //<li>Tom, (10)</li>
+  { id: 2, name: "Joe", score: 20 },
+  { id: 3, name: "Jim", score: 30 },
+  { id: 1, name: "Bob", score: 40 }
+];
+
 Meteor.startup(function _startup() {
   let title = "Test App!";
   let name = "Cathal";
+
+  const List1 = props => (
+    <li>
+      {props.name}, {props.score}
+    </li>
+  );
+
+  const List2 = () => {
+    return LIST.map(p => {
+      return <List1 name={p.name} score={p.score} />;
+    });
+  };
 
   let App = () => {
     return (
       <div>
         <h2>{title}</h2>
         <p>Hello my name is {name}</p>
+
+        <ul>
+          <List2 />
+        </ul>
       </div>
     );
   };
