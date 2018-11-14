6ac1292c8efea31d03a3257b4f5e505b18886728
CathalRyan96 <cathalryan48@gmail.com>
2018-11-14 13:43:46 +0000

    Data from mongodb collecion now appearing on the server

--- client/main.js
+++ client/main.js
@@ -7,10 +7,6 @@ import { Tracker } from "meteor/tracker";
 import { Exercises } from "../import/api/exercises";
 console.log(Exercises.find().fetch());
 
-Tracker.autorun(function _Ready() {
-  console.log("Exercise List: " + Exercises.find().fetch());
-});
-
 const LIST = [
   { id: 1, name: "Press-Ups", set: "5 sets of 5" },
   { id: 2, name: "Press-Ups", set: "5 sets of 5" },
@@ -20,33 +16,36 @@ const LIST = [
 ];
 
 Meteor.startup(function _startup() {
-  let title = "Exercise Tracker";
-  let name = "Cathal";
+  Tracker.autorun(function _Ready() {
+    let title = "Exercise Tracker";
+    let name = "Cathal";
+    let ExerciseList = Exercises.find().fetch();
 
-  const List1 = props => (
-    <li>
-      {props.name}, {props.set}
-    </li>
-  );
-
-  const List2 = () => {
-    return LIST.map(p => {
-      return <List1 key={p.id} {...p} />;
-    });
-  };
-
-  let App = () => {
-    return (
-      <div>
-        <h2>{title}</h2>
-        <p>Hello my name is {name}</p>
-
-        <ul>
-          <List2 />
-        </ul>
-      </div>
+    const List1 = props => (
+      <li>
+        {props.name}, {props.set}
+      </li>
     );
-  };
 
-  ReactDOM.render(<App />, document.getElementById("app"));
+    const List2 = () => {
+      return ExerciseList.map(p => {
+        return <List1 key={p.id} {...p} />;
+      });
+    };
+
+    const App = () => {
+      return (
+        <div>
+          <h2>{title}</h2>
+          <p>Hello my name is {name}</p>
+
+          <ul>
+            <List2 />
+          </ul>
+        </div>
+      );
+    };
+
+    ReactDOM.render(<App />, document.getElementById("app"));
+  });
 });
