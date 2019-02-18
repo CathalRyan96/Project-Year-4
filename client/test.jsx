8ab01c46868e3e8482ce4d5242f157883f85913e
CathalRyan96 <cathalryan48@gmail.com>
2019-02-18 11:00:47 +0000

    added bootstrap packages

--- client/test.jsx
+++ client/test.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import { Button } from "react-bootstrap";
 
 class Test extends Component {
   render() {
     return (
       <div>
         <h1>Select Sports That You Play!</h1>
-        <button>Hurling</button>
+        <Button variant="primary">Primary</Button>
         <button>Soccer</button>
         <button>Rugby</button>
         <button>Gaelic Football</button>
