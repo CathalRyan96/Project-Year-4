c9ddc049b87aa0da770d043f90fab7dfc2b85518
CathalRyan96 <cathalryan48@gmail.com>
2019-03-05 13:07:29 +0000

    added about jsx page

---
+++ client/about.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Button } from "react-bootstrap";
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Information about the application .....</h1>
+      </div>
+    );
+  }
+}
+
+export default Test;
