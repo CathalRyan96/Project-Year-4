d0e91e9e9e57c6d016b308d81abbb7e5481036fc
CathalRyan96 <cathalryan48@gmail.com>
2019-03-24 21:36:18 +0000

    improving navbar

--- client/navbar.jsx
+++ client/navbar.jsx
@@ -15,10 +15,33 @@ const About = () => {
 class NavBar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
         <a className="navbar-brand" href="#">
-          Exercise Application
+          Sport Exercises
         </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <Router>
+              <NavLink to="/" exact activeStyle={{ color: "green" }}>
+                Home
+              </NavLink>
+              <NavLink to="/About" exact activeStyle={{ color: "green" }}>
+                About
+              </NavLink>
+            </Router>
+          </ul>
+        </div>
       </nav>
     );
   }
