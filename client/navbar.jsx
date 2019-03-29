f4a4183076cef176d47c5c998fbd31d5f8d28a95
CathalRyan96 <cathalryan48@gmail.com>
2019-03-29 14:23:15 +0000

    added rugby.js file

--- client/navbar.jsx
+++ client/navbar.jsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import "./about";
 
 import { BrowserRouter as Router, Link, NavLink } from "react-router-dom";
-import Route from "react-router-dom/Route";
 
 const About = () => {
   return (
@@ -15,33 +14,10 @@ const About = () => {
 class NavBar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+      <nav className="navbar navbar-dark bg-dark">
         <a className="navbar-brand" href="#">
-          Sport Exercises
+          Exercise Application
         </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <Router>
-              <NavLink to="/" exact activeStyle={{ color: "green" }}>
-                Home
-              </NavLink>
-              <NavLink to="/About" exact activeStyle={{ color: "green" }}>
-                About
-              </NavLink>
-            </Router>
-          </ul>
-        </div>
       </nav>
     );
   }
