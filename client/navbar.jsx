06c94850343ff07810d6ae41158f9ba2c74ad2ce
CathalRyan96 <cathalryan48@gmail.com>
2019-04-06 12:00:11 +0100

    added githu link

--- client/navbar.jsx
+++ client/navbar.jsx
@@ -39,21 +39,10 @@ export default class Example extends React.Component {
                 <NavLink href="/About">About</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">
+                <NavLink href="https://github.com/CathalRyan96/Project-Year-4">
                   GitHub
                 </NavLink>
               </NavItem>
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
-                </DropdownToggle>
-                <DropdownMenu right>
-                  <DropdownItem>Option 1</DropdownItem>
-                  <DropdownItem>Option 2</DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>Reset</DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
             </Nav>
           </Collapse>
         </Navbar>
